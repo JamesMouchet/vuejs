e3f76eefe58f83e9698016c2f963e75f7fd55dea
James Mouchet <james.mouchet@Digital-Grenoble.local>
2018-03-02 15:33:44 +0100

    premier commit

---
+++ src/App.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.gif">
+    <h1>{{ msg }}</h1>
+    <h2 v-bind:class="{ red: active }">Machine 1</h2> 
+    <label class="switch">
+      <input type="checkbox" v-model="active">
+      <span class="slider"></span>
+    </label>
+    <h2>Machine 2</h2> 
+      <label class="switch">
+        <input type="checkbox">
+        <span class="slider"></span>
+      </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      active: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.red {
+  color: red;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {display:none;}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+img {
+  display: block;
+  margin: auto;
+  width: 50%;
+}
+</style>
